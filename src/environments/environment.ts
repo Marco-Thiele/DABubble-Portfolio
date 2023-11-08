891a7e0f1bbdda008f1e2a03e78c5d78e0d3cec8
Marco-Thiele <125655717+Marco-Thiele@users.noreply.github.com>
2023-11-08 17:30:44 +0100

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyCGqgrOy1nFPvZJTh2NGCA0qiL07a8Sq_U",
+  authDomain: "dabubble-37aeb.firebaseapp.com",
+  projectId: "dabubble-37aeb",
+  storageBucket: "dabubble-37aeb.appspot.com",
+  messagingSenderId: "123942224787",
+  appId: "1:123942224787:web:7d2dca8bfea52c4e6b5e66"
+  },
+};
